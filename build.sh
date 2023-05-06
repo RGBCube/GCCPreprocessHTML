3676ab38b8497c965d6c4b1e6d0adb3c12146c8f
RGBCube <RGBCube@users.noreply.github.com>
2023-05-07 01:07:38 +0300

    Feat: Ignore files that start with _

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ mkdir ./build
 
 for html_file in $(find -name "*.html"); do
   path=./build/${html_file#./}
-  mkdir $(dirname $path) 2> /dev/null
-  sed "/^#/d" <(process $html_file) > $path
+
+  # Preprocess if filename doesn't start with _.
+  if [[ $(basename $html_file) != _* ]]; then
+    echo "Building $html_file..."
+    mkdir $(dirname $path) 2> /dev/null
+    sed "/^#/d" <(process $html_file) > $path
+  fi
 done
