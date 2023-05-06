b07ef3c39c534f8312344399a4cd7a4164196dc6
RGBCube <RGBCube@users.noreply.github.com>
2023-05-07 00:50:31 +0300

    Make it work

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ rm -rf ./build
 mkdir ./build
 
 for html_file in $(find -name "*.html"); do
-  sed "/^#/d" <(process $html_file) > ./build/${html_file#./}
+  path=./build/${html_file#./}
+  mkdir $(dirname $path) 2> /dev/null
+  sed "/^#/d" <(process $html_file) > $path
 done
