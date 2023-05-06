e48a14f1037dd74e55b7fb4c26ce799aaa965d0f
RGBCube <RGBCube@users.noreply.github.com>
2023-05-07 00:42:14 +0300

    Scripts: Fix build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-#!/usr/bin/sh
+#!/bin/sh
+
+function process {
+  cc -E - < "$1"
+}
+
+rm -rf ./build
+mkdir ./build
+
+for html_file in $(find -name "*.html"); do
+  sed "/^#/d" <(process $html_file) > ./build/${html_file#./}
+done
